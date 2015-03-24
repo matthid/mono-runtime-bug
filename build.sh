a38e2d265191a5f77201eec41427098a85392c16
Matthias Dittrich <matthi.d@googlemail.com>
2015-03-24 19:58:48 +0100

    wait for travis failure.

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 xbuild mono-runtime-bug.sln
-mono mono-runtime-bug/bin/Debug/mono-runtime-bug.exe
+set -e
+for i in `seq 1 1000`; do mono mono-runtime-bug/bin/Debug/mono-runtime-bug.exe; done
 
